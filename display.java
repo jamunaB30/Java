0d008a29c18fe3e1c281334bf51f36e7751ab010
jamunaB30 <baskar97319@gmail.com>
2025-07-27 11:36:01 +0530

    display

---
+++ display.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.Scanner;
+class display{
+	public static void main(String args[])
+	{
+		System.out.println("first ten number are:");
+		for (int i=1;i<=10;i++)
+			System.out.println(i);
+	}
+}
